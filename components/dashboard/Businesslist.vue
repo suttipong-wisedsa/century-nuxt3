a690ecbf9375a34b2b943484b5523cdcd9dd4353
suttipong-wisedsa <64350049+suttipong-wisedsa@users.noreply.github.com>
2023-04-19 17:59:04 +0700

    .

--- components/dashboard/Businesslist.vue
+++ components/dashboard/Businesslist.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-container>
-      <v-card>
+      <v-card v-show="slide">
         <div class="d-flex justify-space-between pa-5">
           <h1>Business List</h1>
           <div>
@@ -20,6 +20,7 @@
               prepend-icon="mdi-plus-circle-outline"
               color="black"
               class="ml-5"
+              @click="Create()"
             >
               <template v-slot:prepend>
                 <v-icon color="white"></v-icon>
@@ -34,48 +35,33 @@
           </div>
         </div>
         <div class="pa-5" style="font-size: 20px; font-weight: bold">
-          7 Total
+          {{ desserts.length }} Total
         </div>
-        <!-- <v-data-table
-          :headers="headers"
-          :items="desserts"
-          class="elevation-1"
-          :items-per-page="itemsPerPage"
-          show-select
-        >
-          <template v-slot:item="{ item }">
-            <tr>
-              <td>
-                <v-checkbox :label="item.value.name"></v-checkbox>
-              </td>
-              <td>{{ item.value.calories }}</td>
-              <td>{{ item.value.carbs }}</td>
-              <td>
-                <v-btn
-                  prepend-icon="mdi-magnify"
-                  variant="outlined"
-                  color="#00BAFF"
-                >
-                  See more
-                </v-btn>
-              </td>
-            </tr>
-          </template>
-        </v-data-table> -->
         <div id="app">
-          <v-app>
-            <v-data-table
-              show-select
-              v-model="selected"
-              :headers="headers"
-              :items="desserts"
-              :items-per-page="5"
-              class="elevation-1"
-            >
-              <template v-slot:item.name="{ item }"> </template>
-            </v-data-table>
-          </v-app>
+          <v-data-table
+            :headers="headers"
+            :items="desserts"
+            class="elevation-1"
+            show-select
+            v-model="selected"
+          >
+            <template v-slot:item.glutenfree="{ item }">
+              <v-btn
+                prepend-icon="mdi-magnify"
+                variant="outlined"
+                color="#00BAFF"
+              >
+                See more
+              </v-btn>
+            </template>
+          </v-data-table>
         </div>
+      </v-card>
+      <v-card>
+        <DashboardCreate
+          :slide="slide"
+          @changeB="cancle($event)"
+        ></DashboardCreate>
       </v-card>
     </v-container>
   </div>
@@ -91,59 +77,68 @@ export default {
     const drawer = ref(true);
     const itemsPerPage = ref(5);
     const selected = ref([]);
+    const slide = ref(true);
+    const reveal = ref(false);
     const headers = ref([
       {
         title: "Business Name",
-        align: "left",
+        align: "center",
         sortable: false,
-        value: "name",
+        key: "name",
       },
-      { title: "Services count", value: "calories" },
-      { title: "Date created", value: "fat" },
-      { title: "Action", value: "action" },
+      {
+        title: "Services count",
+        key: "calories",
+        align: "center",
+        sortable: false,
+      },
+      { title: "Date created", key: "fat", align: "center", sortable: false },
+      { title: "Action", key: "glutenfree", align: "center", sortable: false },
     ]);
     const desserts = ref([
       {
         name: "Frozen Yogurt",
         calories: 159,
         fat: 6.0,
-
-        protein: 4.0,
-        iron: "1%",
+        glutenfree: true,
       },
       {
         name: "Frozen Yogurt",
         calories: 159,
         fat: 6.0,
-
-        protein: 4.0,
-        iron: "1%",
+        glutenfree: true,
       },
       {
         name: "Frozen Yogurt",
         calories: 159,
         fat: 6.0,
-
-        protein: 4.0,
-        iron: "1%",
+        glutenfree: true,
       },
       {
         name: "Frozen Yogurt",
         calories: 159,
         fat: 6.0,
-
-        protein: 4.0,
-        iron: "1%",
+        glutenfree: true,
       },
       {
         name: "Frozen Yogurt",
         calories: 159,
         fat: 6.0,
-
-        protein: 4.0,
-        iron: "1%",
+        glutenfree: true,
+      },
+      {
+        name: "Frozen Yogurt",
+        calories: 159,
+        fat: 6.0,
+        glutenfree: true,
       },
     ]);
+    function Create() {
+      slide.value = false;
+    }
+    function cancle(e) {
+      console.log(e);
+    }
     definePageMeta({
       layout: false,
     });
@@ -158,109 +153,11 @@ export default {
       headers,
       itemsPerPage,
       selected,
+      Create,
+      slide,
+      reveal,
+      cancle,
     };
   },
 };
 </script>
-<!-- <script>
-export default {
-  data() {
-    return {
-      itemsPerPage: 5,
-      headers: [
-        {
-          title: "Business Name",
-          align: "start",
-          sortable: false,
-          key: "name",
-        },
-        { title: "Services count", align: "start", key: "calories" },
-        { title: "Data created", align: "start", key: "fat" },
-        { title: "Action", align: "start", key: "carbs" },
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1",
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0",
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6",
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7",
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16",
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1",
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2",
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8",
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45",
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22",
-        },
-      ],
-    };
-  },
-};
-</script> -->
---
+++ components/dashboard/Create.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="pa-5" v-show="slide">
+    <div class="d-flex justify-space-between pb-2">
+      <h1>Create Business</h1>
+      <div>
+        <v-btn
+          prepend-icon="mdi-close"
+          color="red"
+          variant="outlined"
+          @click="cancle()"
+        >
+          <template v-slot:prepend>
+            <v-icon color="black"></v-icon>
+          </template>
+          Cancle
+          <template v-slot:append>
+            <v-icon color="warning"></v-icon>
+          </template>
+        </v-btn>
+        <v-btn
+          prepend-icon="mdi-plus-circle-outline"
+          color="#00BAFF"
+          class="ml-5"
+          @click="Create()"
+        >
+          <template v-slot:prepend>
+            <v-icon color="white"></v-icon>
+          </template>
+
+          <p class="text-white">Create</p>
+
+          <template v-slot:append>
+            <v-icon color="warning"></v-icon>
+          </template>
+        </v-btn>
+      </div>
+    </div>
+    <v-divider :thickness="3"></v-divider>
+    <h3>Business Type*</h3>
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: ["slide"],
+  emits: ["changeB"],
+  setup(props, { emit }) {
+    const slide = ref(props.slide);
+    function cancle() {
+      emit("changeB", { name: "sdsdsd" });
+    }
+    return {
+      slide,
+    };
+  },
+});
+</script>
